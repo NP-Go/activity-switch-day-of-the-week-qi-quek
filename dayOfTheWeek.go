b597ff506a31ffdbb51b2a003f13c142bdf5c27c
narwhal04 <q.quek7@gmail.com>
2022-03-31 18:27:00 +0800

    functioning code

--- dayOfTheWeek.go
+++ dayOfTheWeek.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	//Insert code here
+	today := time.Now().Weekday()
+
+	// fmt.Printf("%v", time.Monday)
+	// fmt.Printf("%v", today)
+
+	switch today {
+	case time.Monday:
+		fmt.Println("Today is ", today)
+	case time.Tuesday:
+		fmt.Println("Today is ", today)
+	case time.Wednesday:
+		fmt.Println("Today is ", today)
+	case time.Thursday:
+		fmt.Println("Today is ", today)
+	case time.Friday:
+		fmt.Println("Today is ", today)
+	default:
+		fmt.Println("Its the weekend")
+	}
 }
